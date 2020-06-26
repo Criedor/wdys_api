6b41b2425077fc100fb83b843394d8300a72054d
criedor <gawen@t-online.de>
2020-06-26 11:16:47 +0200

    fix port settings for heroku

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/', apiRouter);
 
 
-app.listen(port, () =>
+app.listen(port,'0.0.0.0', () =>
 console.log('server is running on port: ' +port))
 
 
